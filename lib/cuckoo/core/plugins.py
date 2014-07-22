293653f4d08e6e26c43428248b3a16ced31a8ccc
tomer <adi.hayon1@gmail.com>
2014-07-22 17:31:25 +0300

    Added change in plugins to preserve changes in task

--- lib/cuckoo/core/plugins.py
+++ lib/cuckoo/core/plugins.py
@@ -134,10 +134,11 @@ class RunProcessing(object):
     is then passed over the reporting engine.
     """
 
-    def __init__(self, task_id):
+    def __init__(self, task):
         """@param task_id: ID of the analyses to process."""
-        self.task = Database().view_task(task_id).to_dict()
-        self.analysis_path = os.path.join(CUCKOO_ROOT, "storage", "analyses", str(task_id))
+        # ADI: (We want to preserve changes in task) self.task = Database().view_task(task_id).to_dict()
+	self.task = task.to_dict()
+        self.analysis_path = os.path.join(CUCKOO_ROOT, "storage", "analyses", str(task.id))
         self.cfg = Config(cfg=os.path.join(CUCKOO_ROOT, "conf", "processing.conf"))
 
     def process(self, module):
